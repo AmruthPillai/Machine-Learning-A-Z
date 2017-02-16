1726beffda41907e7a643729322763dd52e0ed70
Amruth Pillai <im.amruth@gmail.com>
2017-02-16 23:20:48 +0530

    predicting the test data using SLR

--- Part 2 - Regression/01 - Simple Linear Regression/simple_linear_regression.py
+++ Part 2 - Regression/01 - Simple Linear Regression/simple_linear_regression.py
@@ -19,3 +19,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, rando
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+# Predicting the Test Set results
+y_pred = regressor.predict(X_test)
